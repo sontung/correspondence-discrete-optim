b0a77c502cd87893f76857135f6a4149ecc9bd2c
sontung <tung.nguyenson1404@gmail.com>
2021-03-20 19:39:01 +0700

    zncc mat

--- cpd_algo.py
+++ cpd_algo.py
@@ -5,11 +5,10 @@ import numpy as np
 import cv2
 import sys
 from pathlib import Path
+from math_utils import compute_zncc_min_version
 
 
 def write():
-    IM1 = cv2.imread("data/im1.png")
-    IM2 = cv2.imread("data/im2.png")
     IM1_masked = cv2.imread("data/im1maskeds.png")
     IM2_masked = cv2.imread("data/im2maskeds.png")
 
@@ -32,7 +31,6 @@ def write():
 
 
 def visualize(iteration, error, X, Y, ax):
-    print(error)
     plt.cla()
     ax.scatter(X[0:-1:1, 0],  X[0:-1:1, 1], color='red', label='Target')
     ax.scatter(Y[0:-1:1, 0],  Y[0:-1:1, 1], color='blue', label='Source')
@@ -44,16 +42,30 @@ def visualize(iteration, error, X, Y, ax):
 
 
 def main():
+    IM1 = cv2.imread("data/im1.png")
+    IM2 = cv2.imread("data/im2.png")
     X = np.loadtxt('data/target.txt')
     Y = np.loadtxt('data/source.txt')
-    np.random.shuffle(Y)
-    # sys.exit()
+
+    my_file = Path("saved/zncc_mat.npy")
+
+    if my_file.exists():
+        zncc_mat = np.load("saved/zncc_mat.npy")
+    else:
+        zncc_mat = np.zeros((Y.shape[0], X.shape[0]))
+        for i in range(zncc_mat.shape[0]):
+            for j in range(zncc_mat.shape[1]):
+                x, y = map(int, X[j])
+                x2, y2 = map(int, Y[j])
+                zncc = compute_zncc_min_version(x, y, x2, y2, IM1, IM2, 5, debug=False)
+                zncc_mat[i, j] = zncc[0]
+        np.save('saved/zncc_mat.npy', zncc_mat)
 
     fig = plt.figure()
     fig.add_axes([0, 0, 1, 1])
     callback = partial(visualize, ax=fig.axes[0])
 
-    reg = AffineRegistration(**{'X': X, 'Y': Y})
+    reg = AffineRegistration(**{'X': X, 'Y': Y, "zncc_mat": zncc_mat})
     reg.register(callback)
     plt.show()
 

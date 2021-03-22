0c53004eec7c20208d0089e6eb8971035c4ded5c
Son Tung <tungns@hblab.vn>
2021-03-22 17:27:35 +0700

    em coarse to fine

--- cpd_algo.py
+++ cpd_algo.py
@@ -1,6 +1,6 @@
 from functools import partial
 import matplotlib.pyplot as plt
-from pycpd import AffineRegistration
+from em_registration import AffineRegistration
 import numpy as np
 import cv2
 import sys
@@ -9,31 +9,41 @@ from math_utils import compute_zncc_min_version
 
 
 def write():
-    IM1_masked = cv2.imread("data/im1maskeds.png")
-    IM2_masked = cv2.imread("data/im2maskeds.png")
+    IM1 = cv2.imread("data/im1.png")
+    IM2 = cv2.imread("data/im2.png")
+    IM1_masked = cv2.imread("data/im1masked.png")
+    IM2_masked = cv2.imread("data/im2masked.png")
 
     my_file = Path("data/target.txt")
 
     if my_file.is_file():
         return
 
+    colors = []
     sys.stdout = open("data/target.txt", "w")
     for i in range(IM1_masked.shape[0]):
         for j in range(IM1_masked.shape[1]):
             if IM1_masked[i, j, 0] > 0:
                 print(i, j)
+                colors.append(IM1[i, j])
+    colors = np.array(colors)
+    np.save('data/target_colors.npy', colors)
+    colors = []
     sys.stdout = open("data/source.txt", "w")
     for i in range(IM2_masked.shape[0]):
         for j in range(IM2_masked.shape[1]):
             if IM2_masked[i, j, 0] > 0:
                 print(i, j)
+                colors.append(IM2[i, j])
+    colors = np.array(colors)
+    np.save('data/source_colors.npy', colors)
     sys.stdout = sys.__stdout__
 
 
-def visualize(iteration, error, X, Y, ax):
+def visualize(iteration, error, X, Y, X_color, Y_color, ax):
     plt.cla()
-    ax.scatter(X[0:-1:1, 0],  X[0:-1:1, 1], color='red', label='Target')
-    ax.scatter(Y[0:-1:1, 0],  Y[0:-1:1, 1], color='blue', label='Source')
+    ax.scatter(X[0:-1:1, 0],  X[0:-1:1, 1], color=X_color/255.0, label='Target')
+    ax.scatter(Y[0:-1:1, 0],  Y[0:-1:1, 1], color="white", label='Source')
     plt.text(0.87, 0.92, 'Iteration: {:d}\nQ: {:06.4f}'.format(
         iteration, error), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
     ax.legend(loc='upper left', fontsize='x-large')
@@ -41,35 +51,70 @@ def visualize(iteration, error, X, Y, ax):
     plt.pause(0.001)
 
 
+def visualize_transformation(reg):
+    X = np.loadtxt('data/target.txt')
+    Y = np.loadtxt('data/source.txt')
+    X_color = np.load("data/target_colors.npy")
+    Y_color = np.load("data/source_colors.npy")
+
+    Y_transformed = reg.transform_point_cloud(Y)
+    plt.subplot(311)
+    plt.scatter(Y_transformed[0:-1:1, 0],  Y_transformed[0:-1:1, 1], color=Y_color/255.0)
+    plt.subplot(312)
+    plt.scatter(Y[0:-1:1, 0],  Y[0:-1:1, 1], color=Y_color/255.0)
+    plt.subplot(313)
+    plt.scatter(X[0:-1:1, 0],  X[0:-1:1, 1], color=X_color/255.0)
+    plt.show()
+
+
 def main():
     IM1 = cv2.imread("data/im1.png")
     IM2 = cv2.imread("data/im2.png")
     X = np.loadtxt('data/target.txt')
     Y = np.loadtxt('data/source.txt')
+    X_color = np.load("data/target_colors.npy")
+    Y_color = np.load("data/source_colors.npy")
 
-    my_file = Path("saved/zncc_mat.npy")
+    Y = Y[0:-1:100, :]
+    Y_color = Y_color[0:-1:100, :]
 
-    if my_file.exists():
-        zncc_mat = np.load("saved/zncc_mat.npy")
-    else:
-        zncc_mat = np.zeros((Y.shape[0], X.shape[0]))
-        for i in range(zncc_mat.shape[0]):
-            for j in range(zncc_mat.shape[1]):
-                x, y = map(int, X[j])
-                x2, y2 = map(int, Y[j])
-                zncc = compute_zncc_min_version(x, y, x2, y2, IM1, IM2, 5, debug=False)
-                zncc_mat[i, j] = zncc[0]
-        np.save('saved/zncc_mat.npy', zncc_mat)
+    # my_file = Path("saved/zncc_mat.npy")
+    # if my_file.exists():
+    #     zncc_mat = np.load("saved/zncc_mat.npy")
+    # else:
+    #     zncc_mat = np.zeros((Y.shape[0], X.shape[0]))
+    #     for i in range(zncc_mat.shape[0]):
+    #         for j in range(zncc_mat.shape[1]):
+    #             x, y = map(int, X[j])
+    #             x2, y2 = map(int, Y[j])
+    #             zncc = compute_zncc_min_version(x, y, x2, y2, IM1, IM2, 5, debug=False)
+    #             zncc_mat[i, j] = zncc[0]
+    #     np.save('saved/zncc_mat.npy', zncc_mat)
 
     fig = plt.figure()
     fig.add_axes([0, 0, 1, 1])
     callback = partial(visualize, ax=fig.axes[0])
 
-    reg = AffineRegistration(**{'X': X, 'Y': Y, "zncc_mat": zncc_mat})
+    reg = AffineRegistration(**{'X': X, 'Y': Y, 'X_color': X_color, 'Y_color': Y_color})
     reg.register(callback)
     plt.show()
+    # visualize_transformation(reg)
+
+    Y = np.loadtxt('data/source.txt')
+    Y_color = np.load("data/source_colors.npy")
+    Y = Y[0:-1:50, :]
+    Y_color = Y_color[0:-1:50, :]
+    reg.reload_inputs(X, Y, X_color, Y_color)
+    fig = plt.figure()
+    fig.add_axes([0, 0, 1, 1])
+    callback = partial(visualize, ax=fig.axes[0])
+    reg.register(callback)
+    plt.show()
+    visualize_transformation(reg)
+
 
 
 if __name__ == '__main__':
     write()
+    # sys.exit()
     main()
---
+++ em_registration.py
@@ -0,0 +1,310 @@
+import numpy as np
+import numbers
+import sys
+from warnings import warn
+
+
+def initialize_sigma2(X, Y):
+    (N, D) = X.shape
+    (M, _) = Y.shape
+    diff = X[None, :, :] - Y[:, None, :]
+    err = diff ** 2
+    return np.sum(err) / (D * M * N)
+
+
+def is_positive_semi_definite(R):
+    if not isinstance(R, (np.ndarray, np.generic)):
+        raise ValueError('Encountered an error while checking if the matrix is positive semi definite. \
+            Expected a numpy array, instead got : {}'.format(R))
+    return np.all(np.linalg.eigvals(R) > 0)
+
+
+class EMRegistration(object):
+    """
+    Expectation maximization point cloud registration.
+
+    Attributes
+    ----------
+    X: numpy array
+        NxD array of target points.
+
+    Y: numpy array
+        MxD array of source points.
+
+    Y: numpy array
+        MxD array of transformed source points.
+
+    sigma2: float (positive)
+        Initial variance of the Gaussian mixture model.
+
+    N: int
+        Number of target points.
+
+    M: int
+        Number of source points.
+
+    D: int
+        Dimensionality of source and target points
+
+    iteration: int
+        The current iteration throughout registration.
+
+    max_iterations: int
+        Registration will terminate once the algorithm has taken this
+        many iterations.
+
+    tolerance: float (positive)
+        Registration will terminate once the difference between
+        consecutive objective function values falls within this tolerance.
+
+    w: float (between 0 and 1)
+        Contribution of the uniform distribution to account for outliers.
+        Valid values span 0 (inclusive) and 1 (exclusive).
+
+    q: float
+        The objective function value that represents the misalignment between source
+        and target point clouds.
+
+    diff: float (positive)
+        The absolute difference between the current and previous objective function values.
+
+    P: numpy array
+        MxN array of probabilities.
+        P[m, n] represents the probability that the m-th source point
+        corresponds to the n-th target point.
+
+    Pt1: numpy array
+        Nx1 column array.
+        Multiplication result between the transpose of P and a column vector of all 1s.
+
+    P1: numpy array
+        Mx1 column array.
+        Multiplication result between P and a column vector of all 1s.
+
+    Np: float (positive)
+        The sum of all elements in P.
+
+    """
+
+    def __init__(self, X, Y, X_color, Y_color, P=None, sigma2=None, max_iterations=None, tolerance=None, w=None, *args, **kwargs):
+        if type(X) is not np.ndarray or X.ndim != 2:
+            raise ValueError(
+                "The target point cloud (X) must be at a 2D numpy array.")
+
+        if type(Y) is not np.ndarray or Y.ndim != 2:
+            raise ValueError(
+                "The source point cloud (Y) must be a 2D numpy array.")
+
+        if X.shape[1] != Y.shape[1]:
+            raise ValueError(
+                "Both point clouds need to have the same number of dimensions.")
+
+        if sigma2 is not None and (not isinstance(sigma2, numbers.Number) or sigma2 <= 0):
+            raise ValueError(
+                "Expected a positive value for sigma2 instead got: {}".format(sigma2))
+
+        if max_iterations is not None and (not isinstance(max_iterations, numbers.Number) or max_iterations < 0):
+            raise ValueError(
+                "Expected a positive integer for max_iterations instead got: {}".format(max_iterations))
+        elif isinstance(max_iterations, numbers.Number) and not isinstance(max_iterations, int):
+            warn("Received a non-integer value for max_iterations: {}. Casting to integer.".format(max_iterations))
+            max_iterations = int(max_iterations)
+
+        if tolerance is not None and (not isinstance(tolerance, numbers.Number) or tolerance < 0):
+            raise ValueError(
+                "Expected a positive float for tolerance instead got: {}".format(tolerance))
+
+        if w is not None and (not isinstance(w, numbers.Number) or w < 0 or w >= 1):
+            raise ValueError(
+                "Expected a value between 0 (inclusive) and 1 (exclusive) for w instead got: {}".format(w))
+
+        self.X = X
+        self.Y = Y
+        self.X_color = X_color
+        self.Y_color = Y_color
+        self.TY = Y
+        self.sigma2 = initialize_sigma2(X, Y) if sigma2 is None else sigma2
+        (self.N, self.D) = self.X.shape
+        (self.M, _) = self.Y.shape
+        self.tolerance = 0.001 if tolerance is None else tolerance
+        self.w = 0.0 if w is None else w
+        self.max_iterations = 100 if max_iterations is None else max_iterations
+        self.iteration = 0
+        self.diff = np.inf
+        self.q = np.inf
+        self.P = np.zeros((self.M, self.N))
+        self.Pt1 = np.zeros((self.N, ))
+        self.P1 = np.zeros((self.M, ))
+        self.Np = 0
+
+    def register(self, callback=lambda **kwargs: None):
+        self.transform_point_cloud()
+        # while self.iteration < self.max_iterations:
+        while self.iteration < 300:
+            self.iterate()
+            if callable(callback):
+                kwargs = {'iteration': self.iteration,
+                          'error': self.q, 'X': self.X, 'Y': self.TY, "X_color": self.X_color, "Y_color": self.Y_color}
+                callback(**kwargs)
+            if self.q < 0:
+                break
+        return self.TY, self.get_registration_parameters()
+
+    def get_registration_parameters(self):
+        raise NotImplementedError(
+            "Registration parameters should be defined in child classes.")
+
+    def update_transform(self):
+        raise NotImplementedError(
+            "Updating transform parameters should be defined in child classes.")
+
+    def transform_point_cloud(self):
+        raise NotImplementedError(
+            "Updating the source point cloud should be defined in child classes.")
+
+    def update_variance(self):
+        raise NotImplementedError(
+            "Updating the Gaussian variance for the mixture model should be defined in child classes.")
+
+    def iterate(self):
+        self.expectation()
+        self.maximization()
+        self.iteration += 1
+
+    def reload_inputs(self, X, Y, X_color, Y_color):
+        self.X = X
+        self.Y = Y
+        self.X_color = X_color
+        self.Y_color = Y_color
+        (self.M, _) = self.Y.shape
+        self.transform_point_cloud()
+        self.iteration = 0
+        self.diff = np.inf
+        self.q = np.inf
+        self.P = np.zeros((self.M, self.N))
+        self.Pt1 = np.zeros((self.N, ))
+        self.P1 = np.zeros((self.M, ))
+        self.Np = 0
+
+    def expectation(self):
+        P = np.sum((self.X[None, :, :] - self.TY[:, None, :]) ** 2, axis=2)
+
+        c = (2 * np.pi * self.sigma2) ** (self.D / 2)
+        c = c * self.w / (1 - self.w)
+        c = c * self.M / self.N
+
+        P = np.exp(-P / (2 * self.sigma2))
+        den = np.sum(P, axis=0)
+        den = np.tile(den, (self.M, 1))
+        den[den == 0] = np.finfo(float).eps
+        den += c
+
+        self.P = np.divide(P, den)
+        self.Pt1 = np.sum(self.P, axis=0)
+        self.P1 = np.sum(self.P, axis=1)
+        self.Np = np.sum(self.P1)
+
+    def maximization(self):
+        self.update_transform()
+        self.transform_point_cloud()
+        self.update_variance()
+
+
+class AffineRegistration(EMRegistration):
+    """
+    Affine registration.
+
+    Attributes
+    ----------
+    B: numpy array (semi-positive definite)
+        DxD affine transformation matrix.
+
+    t: numpy array
+        1xD initial translation vector.
+
+    YPY: float
+        Denominator value used to update the scale factor.
+        Defined in Fig. 2 and Eq. 8 of https://arxiv.org/pdf/0905.2635.pdf.
+
+    X_hat: numpy array
+        Centered target point cloud.
+        Defined in Fig. 2 of https://arxiv.org/pdf/0905.2635.pdf
+
+    """
+
+    def __init__(self, B=None, t=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if B is not None and (B.ndim is not 2 or B.shape[0] is not self.D or B.shape[1] is not self.D or not is_positive_semi_definite(B)):
+            raise ValueError(
+                'The rotation matrix can only be initialized to {}x{} positive semi definite matrices. Instead got: {}.'.format(self.D, self.D, B))
+
+        if t is not None and (t.ndim is not 2 or t.shape[0] is not 1 or t.shape[1] is not self.D):
+            raise ValueError(
+                'The translation vector can only be initialized to 1x{} positive semi definite matrices. Instead got: {}.'.format(self.D, t))
+        self.B = np.eye(self.D) if B is None else B
+        self.t = np.atleast_2d(np.zeros((1, self.D))) if t is None else t
+
+    def update_transform(self):
+        """
+        Calculate a new estimate of the rigid transformation.
+
+        """
+
+        # source and target point cloud means
+        muX = np.divide(np.sum(np.dot(self.P, self.X), axis=0), self.Np)
+        muY = np.divide(
+            np.sum(np.dot(np.transpose(self.P), self.Y), axis=0), self.Np)
+
+        self.X_hat = self.X - np.tile(muX, (self.N, 1))
+        Y_hat = self.Y - np.tile(muY, (self.M, 1))
+
+        self.A = np.dot(np.transpose(self.X_hat), np.transpose(self.P))
+        self.A = np.dot(self.A, Y_hat)
+
+        self.YPY = np.dot(np.transpose(Y_hat), np.diag(self.P1))
+        self.YPY = np.dot(self.YPY, Y_hat)
+
+        # Calculate the new estimate of affine parameters using update rules for (B, t)
+        # as defined in Fig. 3 of https://arxiv.org/pdf/0905.2635.pdf.
+        self.B = np.linalg.solve(np.transpose(self.YPY), np.transpose(self.A))
+        self.t = np.transpose(
+            muX) - np.dot(np.transpose(self.B), np.transpose(muY))
+
+    def transform_point_cloud(self, Y=None):
+        """
+        Update a point cloud using the new estimate of the affine transformation.
+
+        """
+        if Y is None:
+            self.TY = np.dot(self.Y, self.B) + np.tile(self.t, (self.M, 1))
+            return
+        else:
+            return np.dot(Y, self.B) + np.tile(self.t, (Y.shape[0], 1))
+
+    def update_variance(self):
+        """
+        Update the variance of the mixture model using the new estimate of the affine transformation.
+        See the update rule for sigma2 in Fig. 3 of of https://arxiv.org/pdf/0905.2635.pdf.
+
+        """
+        qprev = self.q
+
+        trAB = np.trace(np.dot(self.A, self.B))
+        xPx = np.dot(np.transpose(self.Pt1), np.sum(
+            np.multiply(self.X_hat, self.X_hat), axis=1))
+        trBYPYP = np.trace(np.dot(np.dot(self.B, self.YPY), self.B))
+        self.q = (xPx - 2 * trAB + trBYPYP) / (2 * self.sigma2) + \
+            self.D * self.Np/2 * np.log(self.sigma2)
+        self.diff = np.abs(self.q - qprev)
+
+        self.sigma2 = (xPx - trAB) / (self.Np * self.D)
+
+        if self.sigma2 <= 0:
+            self.sigma2 = self.tolerance / 10
+
+    def get_registration_parameters(self):
+        """
+        Return the current estimate of the affine transformation parameters.
+
+        """
+        return self.B, self.t
